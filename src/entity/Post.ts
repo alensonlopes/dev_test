dd2b9ccee9e3a86f9cc9727016e11565dd447773
alensonlopes <alenson.hsl@gmail.com>
2024-11-12 08:08:06 -0300

    Create user and post entities

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,19 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { User } from './User';
 
-//TODO Crie a entidade de Post
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ length: 100 })
+  title!: string;
+
+  @Column({ length: 100 })
+  description!: string;
+
+  @Column()
+  userId!: number;
+
+  user!: User;
+}
